fbac2982656aa41e8ccee63e72de24ec289fa6c7
StepashkaSuicide <churbakov.evgen@mail.ru>
2022-09-17 17:58:56 +0300

    commit one test

--- src/features/registration/Registration.tsx
+++ src/features/registration/Registration.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import SuperInputText from '../../common/components/superInputText/SuperInputText';
 
 export const Registration = () => {
 
     return (
-        <div>
-            Registration
-        </div>
+        <form>
+            <SuperInputText/>
+            <SuperInputText/>
+            <SuperInputText/>
+        </form>
     )
 }
 

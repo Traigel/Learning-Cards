5bb8cdb6deff0aa9db9bd58ee9f791d49485d393
TokiLithuwix <loremlisumdol@gmail.com>
2022-09-23 18:33:03 +0700

    styles: styles for Check Email Component

--- src/features/password/checkEmail/CheckEmail.tsx
+++ src/features/password/checkEmail/CheckEmail.tsx
@@ -1,30 +1,31 @@
 import React from 'react'
-import s from './CheckEmail.module.css'
+import styles from './CheckEmail.module.css'
 import SuperButton from '../../../common/components/superButton/SuperButton';
 import checkEmail from '../../../assets/image/checkemail.png'
 import {NavLink} from 'react-router-dom';
 import {useAppSelector} from '../../../common/hooks/hooks';
 
-
 export const CheckEmail = () => {
 
-const email = useAppSelector(s=>s.auth.forgetEmail)
+    const email = useAppSelector(s => s.auth.forgetEmail)
 
-    return <div className={s.containerForm}>
-        <h1 className={s.signUp}>Check Email</h1>
-        <div>
-
-            <div className={s.checkEmail}>
-                <img src={checkEmail} alt={'check'} />
-            </div>
-
-            <div className={s.mail}>
-                <span >{`We’ve sent an Email with instructions to  ${email}`}</span>
-            </div>
-
-            <div className={s.bottomBlock}>
-                <NavLink to={'/login'}><SuperButton className={s.buttonReg} type={'submit'}>Back to login</SuperButton></NavLink>
+    return (
+        <div className={styles.containerForm}>
+            <h1 className={styles.checkMailTitle}>Check Email</h1>
+            <div>
+                <div className={styles.checkEmail}>
+                    <img src={checkEmail} alt={'check'}/>
+                </div>
+                <div className={styles.mail}>
+                    <span className={styles.instructionsTitle}>We’ve sent an Email with instructions to </span>
+                    <span className={styles.emailAddressTitle}>{email}</span>
+                </div>
+                <div className={styles.bottomBlock}>
+                    <NavLink to={'/login'}>
+                        <SuperButton className={styles.buttonReg} type={'submit'}>Back to Login</SuperButton>
+                    </NavLink>
+                </div>
             </div>
         </div>
-    </div>
+    )
 }

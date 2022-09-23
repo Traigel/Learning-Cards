6815d8130d687be7fe7349ae7455aa9fca5368ab
TokiLithuwix <loremlisumdol@gmail.com>
2022-09-23 15:01:47 +0700

    styles: profile component - new styles completed

--- src/common/components/superEditableSpan/SuperEditableSpan.tsx
+++ src/common/components/superEditableSpan/SuperEditableSpan.tsx
@@ -43,7 +43,7 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
 
         onBlur && onBlur(e)
     }
-    const onDoubleClickCallBack = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    const onClickCallBack = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
         setEditMode(true) // включить editMode при двойном клике
 
         onDoubleClick && onDoubleClick(e)
@@ -63,12 +63,13 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
                     />
                 ) : (
                     <span
-                        onDoubleClick={onDoubleClickCallBack}
+                        onClick={onClickCallBack}
                         className={restProps.spanClassName}
                         {...restSpanProps}
-                    >&#9998;
+                    >
                         {/*если нет захардкодженного текста для спана, то значение инпута*/}
                         {children || restProps.value || spanValue}
+                        <span><svg width="35px" height="33px" viewBox="0 -0.08 20 20" data-name="Capa 1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"><path d="M15.51,14.21,12,11.85,13.6,9.49a.38.38,0,0,0-.34-.58l-5,.38A.37.37,0,0,0,8,9.45a.36.36,0,0,0-.05.33l1.56,4.79a.37.37,0,0,0,.3.26h.05a.34.34,0,0,0,.31-.17l1.47-2.19,3.48,2.36a.36.36,0,0,0,.2.06.4.4,0,0,0,.32-.16A.37.37,0,0,0,15.51,14.21ZM10,13.6,8.79,10l3.76-.28Z"/><path d="M6.46,11.18l-1.62.24a.37.37,0,0,0-.32.42.38.38,0,0,0,.37.32h.05l1.62-.23a.38.38,0,0,0,.32-.43A.38.38,0,0,0,6.46,11.18Z"/><path d="M6.1,8.41a.36.36,0,0,0,.32-.18.38.38,0,0,0-.13-.52l-1.4-.83A.38.38,0,0,0,4.37,7a.38.38,0,0,0,.13.51l1.41.84A.45.45,0,0,0,6.1,8.41Z"/><path d="M9.19,7a.36.36,0,0,0,.36.29h.08a.37.37,0,0,0,.29-.45l-.34-1.6A.38.38,0,0,0,9.14,5a.38.38,0,0,0-.29.45Z"/></svg></span>
                     </span>
                 )
             }
--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -35,7 +35,7 @@ export const Profile = () => {
         }
     }
 
-    useEffect(()=> {
+    useEffect(() => {
         if (profileInfo) {
             setInputValue(profileInfo.name)
         }
@@ -49,22 +49,24 @@ export const Profile = () => {
         <div className={styles.profileContainer}>
             <div className={styles.profileBlock}>
                 <h2 className={styles.profileMainTitle}>Personal Information</h2>
-                <div>
+                <div className={styles.userAvatarContainer}>
                     <img className={styles.userAvatar}
                          src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
-                         alt='user-ava'/>
-                    <span>1</span>
+                         alt='user-avatar'/>
+                    <span className={styles.changeAvatarIcon}><svg width="30px" height="30px" viewBox="0 0 52 52"><g><path d="M26,20c-4.4,0-8,3.6-8,8s3.6,8,8,8s8-3.6,8-8S30.4,20,26,20z"/><path d="M46,14h-5.2c-1.4,0-2.6-0.7-3.4-1.8l-2.3-3.5C34.4,7,32.7,6,30.9,6h-9.8c-1.8,0-3.5,1-4.3,2.7l-2.3,3.5c-0.7,1.1-2,1.8-3.4,1.8H6c-2.2,0-4,1.8-4,4v24c0,2.2,1.8,4,4,4h40c2.2,0,4-1.8,4-4V18C50,15.8,48.2,14,46,14z M26,40c-6.6,0-12-5.4-12-12s5.4-12,12-12s12,5.4,12,12S32.6,40,26,40z"/></g></svg></span>
                 </div>
-                <SuperEditableSpan
-                    inputValue={inputValue}
-                    spanValue={userName}
-                    className={styles.additionalForInput}
-                    spanClassName={styles.additionalForSpan}
-                    onChangeText={(e) => changeUserNameHandler(e)}
-                    onBlur={(e) => onBlurHandler(e)}
-                    onKeyPress={(e) => onKeyPressHandler(e)}
-                />
-                <h3>{profileInfo && profileInfo.email}</h3>
+                <div className={styles.inputContainer}>
+                    <SuperEditableSpan
+                        inputValue={inputValue}
+                        spanValue={userName}
+                        className={styles.additionalForInput}
+                        spanClassName={styles.additionalForSpan}
+                        onChangeText={(e) => changeUserNameHandler(e)}
+                        onBlur={(e) => onBlurHandler(e)}
+                        onKeyPress={(e) => onKeyPressHandler(e)}
+                    />
+                </div>
+                <h3 className={styles.userMailTitle}>{profileInfo && profileInfo.email}</h3>
                 <SuperButton onClick={logoutHandler}>
                     Log out
                 </SuperButton>

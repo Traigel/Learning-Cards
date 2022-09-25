05c6f97808f1c64692d78790fef07436514b720b
TokiLithuwix <loremlisumdol@gmail.com>
2022-09-26 04:42:12 +0700

    feat: packs component created, add table for card packs data

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,10 +2,12 @@ import {applyMiddleware, combineReducers, legacy_createStore} from "redux";
 import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk'
 import {AppActionType, appReducer} from "./app-reducer";
 import {AuthActionsType, authReducer} from "../features/login/auth-reducer";
+import {PacksActionsType, packsReducer} from "../features/packs/packs-reducer";
 
 const rootReducer = combineReducers({
     app: appReducer,
     auth: authReducer,
+    packs: packsReducer,
 })
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
@@ -13,7 +15,7 @@ export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 // все типы экшенов для всего приложения
-export type AppRootActionsType = AppActionType | AuthActionsType
+export type AppRootActionsType = AppActionType | AuthActionsType | PacksActionsType
 
 //типизация санки если она возвращает другую санку
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppRootActionsType>
